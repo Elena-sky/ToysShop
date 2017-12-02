f496f9ea301b6b00372d1dd89179f30ffc787487
MacBook <alonasoroka1993@gmail.com>
2017-12-03 00:58:46 +0200

    new commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,49 @@
     <!-- Page Content -->
     <div class="container">
 
-        <!-- Jumbotron Header -->
-        <header class="jumbotron my-4">
-            <h1 class="display-3">A Warm Welcome!</h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt
-                possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam
-                repellat.</p>
-            <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
-        </header>
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="#" alt="Los Angeles" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="ny.jpg" alt="New york" style="width:100%;">
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+    {{--<!-- Jumbotron Header -->--}}
+    {{--<header class="jumbotron my-4">--}}
+    {{--<h1 class="display-3">A Warm Welcome!</h1>--}}
+    {{--<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt--}}
+    {{--possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam--}}
+    {{--repellat.</p>--}}
+    {{--<a href="#" class="btn btn-primary btn-lg">Call to action!</a>--}}
+    {{--</header>--}}
 
         <!-- Page Features -->
         <div class="row text-center">
