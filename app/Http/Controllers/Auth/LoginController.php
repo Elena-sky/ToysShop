639a1aacb6ac49661c3729de8cd584dfac507ff4
MacBook <alonasoroka1993@gmail.com>
2018-01-15 01:21:32 +0200

    login-register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -34,6 +35,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        //$this->middleware(['guest', 'cartWorker'])->except('logout');
         $this->middleware('guest')->except('logout');
     }
 }
