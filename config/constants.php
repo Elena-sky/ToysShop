338c383b8e5d7f873ca5d5c6e3b0c0923ef9f39d
MacBook <alonasoroka1993@gmail.com>
2018-02-24 01:03:15 +0200

    file constants.php

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'paginate' => [
+        'admin_product' => '10',
+        'category_left' => '6',
+
+    ],
+
+    'limit' => [
+        'last_new_goods' => '8',
+    ],
+];
